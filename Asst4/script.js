05e92159104bfd9022d77426054789839e4d894d
Jamil Madanat <jameel139@hotmail.com>
2019-01-07 23:57:10 -0500

    Update script.js

--- Asst4/script.js
+++ Asst4/script.js
@@ -43,15 +43,22 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var i=0; i<names.length; i++ ) {
 
+  
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
+  // Use the string object's 'charAt' function. 
+  
+  var firstLetter=names[i].charAt(0).toLowerCase();
+// expected output: "The character at index 4 is q"
+  //Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
+// expected output: "the quick brown fox jumped over the lazy dog."
+  
   // var firstLetter =
 
   // STEP 12:
@@ -59,10 +66,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firstLetter == "j") {
+    byeSpeaker.speak(names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
   }
 }
 
