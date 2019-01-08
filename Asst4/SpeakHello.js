e83cb737ec44210af2dd49c2acae73a6a58ae74d
Jamil Madanat <jameel139@hotmail.com>
2019-01-07 23:17:52 -0500

    Update SpeakHello.js

--- Asst4/SpeakHello.js
+++ Asst4/SpeakHello.js
@@ -12,7 +12,7 @@ var helloSpeaker = {
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
   // helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  function speak(name) {
+  speak: function (name) {
   console.log(speakWord + " " + name);
   }
  
@@ -27,6 +27,7 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+ window.helloSpeaker=helloSpeaker;
 
 })
 
